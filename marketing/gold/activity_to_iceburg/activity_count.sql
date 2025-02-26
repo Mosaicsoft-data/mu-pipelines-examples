9d9784e1fc24a7ff33e4b07285647ceee54b0e03
keyurishahdev <keyurishahdev@gmail.com>
2025-02-26 16:51:58 -0600

    updates

--- marketing/gold/activity_to_iceburg/activity_count.sql
+++
@@ -1,5 +0,0 @@
-select contact.*
-from 
-crm.contact contact
-left join crm.account account on contact.account_id = account.id 
-where account.id is not null 
--- sales/app_args.py
+++
@@ -1,5 +0,0 @@
-from mu_pipelines_driver.__main__ import main
-
-main()
-
-#spark-submit /home/scripts/app_args.py --global-properties /home/scripts/global-properties.json --connection-properties /home/scripts/connection-properties.json /home/scripts/raw/people/people.json
